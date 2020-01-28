8133d4b00464c72eda378c78a700cd4b5f533ef2
pelop <pelop@RayDark>
2020-01-28 20:17:57 +0100

    Versión corregida

---
+++ src/gps/Bicicleta.java
@@ -0,0 +1,108 @@
+package gps;
+/**
+ * Bicicleta class
+ * 
+ * @Java IES Severo Ochoa
+ * @2.0
+ */
+
+public class Bicicleta {
+    private String marca;
+	private boolean luces;
+	private int ruedas;
+	private static Bicicleta bici;
+
+	public Bicicleta(){
+        setRuedas(4);
+        setLuces(false);
+        setMarca("Orbea");
+    }
+    
+    public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
+    {
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
+        
+        for(int i=0; i<camino1.length; i++)
+        {
+            sum1 += camino1[i];
+        }
+        
+        for(int i=0; i<camino2.length; i++)
+        {
+
+            sum2 += camino2[i];
+        }
+        
+        for(int i=0; i<camino3.length; i++)
+        {
+            sum3 += camino3[i];
+        }
+        
+        if(sum1 < sum2 && sum1 < sum3)
+        {
+            return "camino 1"; 
+        }else if(sum2 < sum3 && sum2 < sum1)
+        {
+            return "camino 2";
+        }else if(sum3 < sum1 && sum3 < sum2)
+        {
+            return "camino 3";
+        }else{
+            if(sum1 == sum2 && sum2 == sum3)
+            {
+                return "camino 1, 2 y 3";
+            }else if(sum1 == sum3)
+            {
+                return "caminos 1 y 3";
+            }else if(sum2 == sum3){
+                return "caminos 2 y 3";
+            }else{
+                return "caminos 1 y 2";
+            }
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        setBici(new Bicicleta());
+        int[] camino1 = {12,34,21,46,25};
+        int[] camino2 = {24,1,5,64,10,15,21};
+        int[] camino3 = {1,5,6,7,8,10,20,46,104};
+        System.out.println("El camino más corto es: "+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
+    }
+
+	public static Bicicleta getBici() {
+		return bici;
+	}
+
+	public static void setBici(Bicicleta bici) {
+		Bicicleta.bici = bici;
+	}
+
+	public int getRuedas() {
+		return ruedas;
+	}
+
+	public void setRuedas(int ruedas) {
+		this.ruedas = ruedas;
+	}
+
+	public boolean isLuces() {
+		return luces;
+	}
+
+	public void setLuces(boolean luces) {
+		this.luces = luces;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+}
+
